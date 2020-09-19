a9d6630af696cf80baec84de55e6d4071291b6a4
Bryon Severns <bryon.severns@gmail.com>
2020-09-19 15:24:29 -0500

    First commit

---
+++ portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
+
+    def __str__(self):     # insteaed of Project object(1)
+        return self.title  # returns title of Project
+                           # like My First Portfolio
